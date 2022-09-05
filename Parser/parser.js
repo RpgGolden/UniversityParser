aa0785f0a661e62d489baba26987a259f8c4e6b8
siren057 <89363282+siren057@users.noreply.github.com>
2022-09-05 10:30:21 +0300

    new bracnh was created

---
+++ Parser/parser.js
@@ -0,0 +1,92 @@
+const { parse } = require("dotenv");
+const { JSDOM } = require("jsdom");
+
+const axios = require("axios");
+
+
+module.exports = {
+    /**
+     * method to parse priem.unecon.ru
+     */
+    UNECON: parseUnecon = async (searchedSNILS) => {
+        const url = `https://priem.unecon.ru/stat/stat_konkurs.php?y=2021&filial_kod=1&zayav_type_kod=1&obr_konkurs_kod=0&recomend_type=null&rec_status_kod=all&ob_forma_kod=1&ob_osnova_kod=1&konkurs_grp_kod=3747&prior=all&status_kod=all&has_agreement=all&show=%D0%9F%D0%BE%D0%BA%D0%B0%D0%B7%D0%B0%D1%82%D1%8C`;
+
+        try {
+            const response = await axios.get(url);
+
+            const dom = new JSDOM(response.data);
+
+            // get tables from page
+            let tables = Array.from(dom.window.document.querySelectorAll('#spisok table'));
+
+            for (const table of tables) {
+                // find indexes of required columns
+                const headRowCells = Array.from(table.querySelectorAll('thead tr td'));
+                const snilsColumnIndex = headRowCells.findIndex(x => x.textContent.trim() === 'СНИЛС');
+                const agreementColumnIndex = headRowCells.findIndex(x => x.textContent.trim() === 'Согласие принято');
+
+                const rows = Array.from(table.querySelectorAll('tbody tr'))
+
+                // finding row with required SNILS
+                const foundRow = rows.find(row => row.querySelectorAll('td')[snilsColumnIndex].textContent.trim() === searchedSNILS);
+
+                // if found - return with agreement state
+                if (foundRow) {
+                    return {
+                        univ: 'СПБГЭУ',
+                        typeofStudy: 'Очная',
+                        studyDir: 'Информационные системы и технологии',
+                        snils: searchedSNILS,
+                        agreement: foundRow.querySelectorAll('td')[agreementColumnIndex].textContent.trim() === '+'
+                    }
+                }
+            }
+
+            // if no rows with SNILS found - return empty result
+            return null;
+        } catch (e) {
+            console.error("Unable to parse unecon.ru", e)
+        }
+    },
+
+    SFEDU: parseSFEDU = async (searchedSNILS) => {
+        const url = `https://sfedu.ru/abitur/list/rus/list_b/09.03.04_%D0%9A%D0%A2_%D0%9E_%D0%93%D0%91.62`;
+
+        try {
+            const response = await axios.get(url);
+
+            const dom = new JSDOM(response.data);
+
+            // get tables from page
+            let tables = Array.from(dom.window.document.querySelectorAll('#spisok table'));
+
+            for (const table of tables) {
+                // find indexes of required columns
+                const headRowCells = Array.from(table.querySelectorAll('thead tr td'));
+                const snilsColumnIndex = headRowCells.findIndex(x => x.textContent.trim() === 'Уникальный код');
+                const agreementColumnIndex = headRowCells.findIndex(x => x.textContent.trim() === 'Согласие на зачисление');
+
+                const rows = Array.from(table.querySelectorAll('tbody tr'))
+
+                // finding row with required SNILS
+                const foundRow = rows.find(row => row.querySelectorAll('td')[snilsColumnIndex].textContent.trim() === searchedSNILS);
+
+                // if found - return with agreement state
+                if (foundRow) {
+                    return {
+                        university: "ЮФУ",
+                        dirOfStudy: "Программная инженерия",
+                        formOfStudy: "Очная",
+                        snils: searchedSNILS,
+                        agreement: foundRow.querySelectorAll('td')[agreementColumnIndex].textContent.trim() === '✓'
+                    }
+                }
+            }
+
+            // if no rows with SNILS found - return empty result
+            return null;
+        } catch (e) {
+            console.error("Unable to parse sfedu.ru", e);
+        }
+    },
+}
---
+++ Post.js
@@ -0,0 +1,103 @@
+// Frameworks
+var express = require('express');
+const mysql = require("mysql2");
+var bodyParser = require('body-parser');
+const urlencodedParser = express.urlencoded({ extended: false });
+const session = require('express-session');
+const parsers = require(__dirname + '/Parser/parser.js');
+
+// MySIL DataBase with users
+const connection = mysql.createConnection({
+    connectionLimit: 5,
+    host: "localhost",
+    user: "root",
+    database: "users",
+    password: "TAGANROG"
+});
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static(__dirname + '/Pages'));
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+var port = 9000;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/Pages/auth.html')
+})
+
+app.post('/' || '/auth.html', async function (req, res) {
+            let email = req.body.email
+            let password = req.body.auth_pass
+
+            if (email && password) {
+                connection.query('SELECT * FROM mails WHERE mail = ? AND pass = ?', [email, password], function (err, results, fields) {
+                    if (err) throw err  
+
+                    if (results.length > 0) {
+                        req.session.loggedin = true;
+                        req.session.email = email;
+                        res.redirect('/snilsSearch.html')
+                    }
+                    else res.send('<script>alert("Неверные почта и/или пароль"); window.location.href = "/"; </script>');
+
+                    res.end()
+                });
+            } else {
+                res.send('<script>alert("Пожалуйста, введите почту и пароль"); window.location.href = "/";</script>');
+                res.end()
+            }
+
+})
+
+app.get('/snilsSearch.html', async function (req, res) {
+    // receive parameters
+    const snils = (req.query['snils'] || '').toString().trim();
+    // validation
+    if (!snils) {
+        res.status(400);
+        res.json({
+            error: "snils is required"
+        });
+
+        return;
+    }
+
+    if (!snils.match(/^\d{3}-\d{3}-\d{3} \d{2}$/)) {
+        res.status(400);
+        res.json({
+            error: "invalid snils format"
+        });
+
+        return;
+    }
+
+    //TODO: Добавить обработку ошибок с парсеров
+
+    // business logic
+
+    //TODO: Одна ошибка кидает ошибки на всё, обсудить/Обработать.
+    const promises = [];
+    for (const key of Object.keys(parsers)) {
+        promises.push(
+            parsers[key](snils)
+        );
+    }
+
+    const result = await Promise.all(promises);
+
+    // preparing results
+
+    // send result
+    res.json(result);
+});
+// start the server
+app.listen(port);
+console.log('Server started! At http://localhost:' + port);
+// Проверяем пароль при входе в аккаунт
