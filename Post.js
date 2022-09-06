0dbe3a411bc7d63d607938d4e187ad3ae7aafaba
RpgGolden <4erepaxa45@mail.ru>
2022-09-06 22:35:41 +0300

    SnilsOutput

--- Post.js
+++ Post.js
@@ -1,103 +1,107 @@
-// Frameworks проверка 11
+// Frameworks
 var express = require('express');
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 var bodyParser = require('body-parser');
 const urlencodedParser = express.urlencoded({ extended: false });
 const session = require('express-session');
 const parsers = require(__dirname + '/Parser/parser.js');
 
-// MySIL DataBase with users
-const connection = mysql.createConnection({
-    connectionLimit: 5,
-    host: "localhost",
-    user: "root",
-    database: "users",
-    password: "TAGANROG"
-});
+(async () => {
+    // MySQL DataBase with users
+    const connection = await mysql.createConnection({
+        connectionLimit: 5,
+        host: "localhost",
+        user: "root",
+        database: "userid",
+        password: "7623dfgh"
+    });
 
-var app = express();
+    var app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(express.static(__dirname + '/Pages'));
-app.use(session({
-    secret: 'secret',
-    resave: true,
-    saveUninitialized: true
-}));
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(express.json());
+    app.use(express.static(__dirname + '/Pages', { extensions: ['html'] }));
 
-var port = 9000;
+    app.use(session({
+        secret: 'secret',
+        resave: true,
+        saveUninitialized: true
+    }));
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/Pages/auth.html')
-})
+    var port = 9000;
 
-app.post('/' || '/auth.html', async function (req, res) {
-            let email = req.body.email
-            let password = req.body.auth_pass
+    app.get('/', (req, res) => {
+        res.sendFile(__dirname + '/Pages/auth.html')
+    })
 
-            if (email && password) {
-                connection.query('SELECT * FROM mails WHERE mail = ? AND pass = ?', [email, password], function (err, results, fields) {
-                    if (err) throw err  
+    app.post('/', async function (req, res) {
+        let email = req.body.email
+        let password = req.body.auth_pass
 
-                    if (results.length > 0) {
-                        req.session.loggedin = true;
-                        req.session.email = email;
-                        res.redirect('/snilsSearch.html')
-                    }
-                    else res.send('<script>alert("Неверные почта и/или пароль"); window.location.href = "/"; </script>');
+        if (email && password) {
+            try {
+                const results = await connection.query('SELECT * FROM users WHERE mail = ? AND pass = ?', [email, password]);
 
-                    res.end()
-                });
-            } else {
-                res.send('<script>alert("Пожалуйста, введите почту и пароль"); window.location.href = "/";</script>');
-                res.end()
+                if (results.length > 0) {
+                    req.session.loggedin = true;
+                    req.session.email = email;
+                    res.redirect('/snilsSearch');
+                }
+                else res.send('<script>alert("Неверные почта и/или пароль"); window.location.href = "/"</script>'); //сломал, починить
+
+                res.end();
+            } catch (err) {
+                console.error(err);
+                res.status(500).send();
             }
+        } else {
+            res.send('<script>alert("Пожалуйста, введите почту и пароль"); window.location.href = "/"</script>');
+            res.end()
+        }
 
-})
+    })
 
-app.get('/snilsSearch.html', async function (req, res) {
-    // receive parameters
-    const snils = (req.query['snils'] || '').toString().trim();
-    // validation
-    if (!snils) {
-        res.status(400);
-        res.json({
-            error: "snils is required"
-        });
+    app.get('/getResult', async function (req, res) {
 
-        return;
-    }
+        // receive parameters
+        const snils = (req.query['snils'] || '').toString().trim();
+        // validation
+        if (!snils) {
+            res.status(400);
+            res.json({
+                error: "snils is required"
+            });
 
-    if (!snils.match(/^\d{3}-\d{3}-\d{3} \d{2}$/)) {
-        res.status(400);
-        res.json({
-            error: "invalid snils format"
-        });
+            return;
+        }
 
-        return;
-    }
+        if (!snils.match(/^\d{3}-\d{3}-\d{3} \d{2}$/)) {
+            res.status(400);
+            res.json({
+                error: "invalid snils format"
+            });
 
-    //TODO: Добавить обработку ошибок с парсеров
+            return;
+        }
 
-    // business logic
+        //TODO: Одна ошибка кидает ошибки на всё, обсудить/Обработать.
+        const promises = [];
+        for (const key of Object.keys(parsers)) {
+            promises.push(
+                parsers[key](snils)
+            );
+        }
 
-    //TODO: Одна ошибка кидает ошибки на всё, обсудить/Обработать.
-    const promises = [];
-    for (const key of Object.keys(parsers)) {
-        promises.push(
-            parsers[key](snils)
-        );
-    }
+        const result = await Promise.all(promises);
 
-    const result = await Promise.all(promises);
+        // preparing results
 
-    // preparing results
+        // send result
+        res.json(result);
+    });
 
-    // send result
-    res.json(result);
-});
-// start the server
-app.listen(port);
-console.log('Server started! At http://localhost:' + port);
-// Проверяем пароль при входе в аккаунт
+    // start the server
+    app.listen(port);
+    console.log('Server started! At http://localhost:' + port);
+    // Проверяем пароль при входе в аккаунт
+})();
